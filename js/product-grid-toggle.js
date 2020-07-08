e7a9486a8d4a7836061b9de2d944e67640afbd8c
Kristina <gorobetskristina@gmail.com>
2020-07-07 21:06:07 -0700

    Updated pages with media queries

--- js/product-grid-toggle.js
+++ js/product-grid-toggle.js
@@ -20,3 +20,53 @@ function showCoffee() {
         coffeeListing2.style.display = "none";
     }
 }
+
+function showTea() {
+    
+    var teaCheckbox = document.getElementById("teaCheckbox");
+    
+    var teaListing = document.getElementById("tea-listing");
+    
+    
+    if (teaCheckbox.checked == true) {
+        teaListing.style.display = "flex";
+    } else {
+        teaListing.style.display = "none";
+    }
+}
+
+function showSweets() {
+    
+    var sweetsCheckbox = document.getElementById("sweetsCheckbox");
+    
+    var sweetsListing = document.getElementById("sweets-listing-1");
+    
+    
+    if (sweetsCheckbox.checked == true) {
+        sweetsListing.style.display = "flex";
+    } else {
+        sweetsListing.style.display = "none";
+    }
+}
+
+function showGifts() {
+    
+    var giftsCheckbox = document.getElementById("giftsCheckbox");
+    
+    var giftsListing = document.getElementById("gifts-listing");
+    
+    var giftsListing2 = document.getElementById("gifts-listing-2");
+    
+    
+    if (giftsCheckbox.checked == true) {
+        giftsListing.style.display = "flex";
+    } else {
+        giftsListing.style.display = "none";
+    }
+    
+    if (giftsCheckbox.checked == true) {
+        giftsListing2.style.display = "flex";
+    } else {
+        giftsListing2.style.display = "none";
+    }
+}
